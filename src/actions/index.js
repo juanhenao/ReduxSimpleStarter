d4581c215f69857398d9e07767299152da466646
Juan Henao <juanhenao10@gmail.com>
2017-02-20 21:05:50 +0100

    Adding redux promises

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API_KEY = '9d412ea280974b35dd3d9f36a75af168';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
