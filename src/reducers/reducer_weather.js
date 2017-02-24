73435fe23e5ca738cb866096f4180e133843a1ab
Juan Henao <jc.hernandez@innovationmecom.de>
2017-02-24 11:02:14 +0100

    New weather container

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,13 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+	switch (action.type) {
+		case FETCH_WEATHER:
+			//Always return a new state in React and redux. Never manipulate state
+			return [ action.payload.data, ...state ]
+			break;
+		default:
+
+	}
 	return state;
 }
